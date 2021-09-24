65ac70952ec4a94fd9959772bc17a9e15cc6232e
Brixter Porras <brix@xofytech.com>
2021-09-24 14:24:38 +0800

    userInput

--- main.go
+++ main.go
@@ -1,67 +1,22 @@
 package main
 
-import "fmt"
-
-// ? capital leter for public
-const LoginToken string = "adfasdfasdfsad"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	// ! String
-	var username string = "brix101"
+	welcome := "Welcome to user input"
+	fmt.Println(welcome)
 
-	fmt.Println(username)
-	// ? printf for placeholder
-	fmt.Printf("Variable is of type: %T \n", username)
+	// ! userinput
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our Pizza")
 
-	// ! Boolean
-	var isLoggedIn bool = false
-	fmt.Println(isLoggedIn)
-	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+	//? comma ok || err ok
 
-	// ! integer
-	// uint8       the set of all unsigned  8-bit integers (0 to 255)
-	// uint16      the set of all unsigned 16-bit integers (0 to 65535)
-	// uint32      the set of all unsigned 32-bit integers (0 to 4294967295)
-	// uint64      the set of all unsigned 64-bit integers (0 to 18446744073709551615)
-
-	// int8        the set of all signed  8-bit integers (-128 to 127)
-	// int16       the set of all signed 16-bit integers (-32768 to 32767)
-	// int32       the set of all signed 32-bit integers (-2147483648 to 2147483647)
-	// int64       the set of all signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
-
-	// complex64   the set of all complex numbers with float32 real and imaginary parts
-	// complex128  the set of all complex numbers with float64 real and imaginary parts
-
-	// byte        alias for uint8
-	// rune        alias for int32
-	var smallVal uint8 = 255
-	fmt.Println(smallVal)
-	fmt.Printf("Variable is of type: %T \n", smallVal)
-
-	//! Float
-	// float32 or 5 digit decemal place    the set of all IEEE-754 32-bit floating-point numbers
-	// float64 or more decemal place    the set of all IEEE-754 64-bit floating-point numbers
-
-	var smallFloat float64 = 255.21212125454
-	fmt.Println(smallFloat)
-	fmt.Printf("Variable is of type: %T \n", smallFloat)
-
-	// ! default values and some aliases
-
-	var x int
-	fmt.Println("default value of int:", x)
-	fmt.Printf("Variable is of type: %T \n", x)
-
-	// ! implicit type
-	var sample = "asdfasdf"
-	fmt.Println(sample)
-
-	// ! no var style || walrus operator not allowed outside method
-	newSample := 343434.23
-	fmt.Println(newSample)
-
-	// ! Logintoken public sample
-	fmt.Println(LoginToken)
-	fmt.Printf("Variable is of type: %T \n", LoginToken)
-
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("Type of this rating is %T", input)
 }
